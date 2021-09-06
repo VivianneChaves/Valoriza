fa8d61f7fc2bc2fd5c09194fb8539c03d900626b
Vivianne Miranda <vivianne.chaves17@gmail.com>
2021-09-05 21:50:53 -0300

    Criação da estrutura Tag

--- src/database/migrations/1630886782966-CreateTags.ts
+++ src/database/migrations/1630886782966-CreateTags.ts
@@ -1,11 +1,11 @@
-import { MigrationInterface, QueryRunner, Table, Timestamp } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateUsers1630871270529 implements MigrationInterface {
+export class CreateTags1630886782966 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "users",
+                name: "tags",
                 columns: [
                     {
                         name: "id",
@@ -15,15 +15,6 @@ export class CreateUsers1630871270529 implements MigrationInterface {
                     {
                         name: "name",
                         type: "varchar"
-                    },
-                    {
-                        name: "email",
-                        type: "varchar"
-                    },
-                    {
-                        name: "admin",
-                        type: "boolean",
-                        default: false
                     },
                     {
                         name: "created_at",
@@ -38,11 +29,10 @@ export class CreateUsers1630871270529 implements MigrationInterface {
                 ],
             })
         );
-
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("users");
+        await queryRunner.dropTable("tags");
     }
 
 }
